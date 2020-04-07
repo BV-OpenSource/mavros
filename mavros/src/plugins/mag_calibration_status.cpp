9da55c6082116668f02a360b6fde8f390a6e341d
André <andre.ferreira@pdmfc.com>
2020-04-07 16:47:16 +0100

    Add mavros plugin to get feedback of compass calibration.

---
+++ mavros/src/plugins/mag_calibration_status.cpp
@@ -0,0 +1,91 @@
+/**
+ * @brief MagCalStatus plugin
+ * @file MagCalStatus.cpp
+ * @author Vladimir Ermakov <vooon341@gmail.com>
+ *
+ * @example MagCalStatus.cpp
+ * @addtogroup plugin
+ * @{
+ */
+/*
+ * Copyright 2013,2016 Vladimir Ermakov.
+ *
+ * This file is part of the mavros package and subject to the license terms
+ * in the top-level LICENSE file of the mavros repository.
+ * https://github.com/mavlink/mavros/tree/master/LICENSE.md
+ */
+
+#include <mavros/mavros_plugin.h>
+#include <std_msgs/UInt8.h>
+
+namespace mavros {
+namespace std_plugins {
+
+/**
+ * @brief MagCalStatus plugin.
+ *
+ * Example and "how to" for users.
+ */
+class MagCalStatusPlugin : public plugin::PluginBase {
+public:
+	MagCalStatusPlugin() : PluginBase(),
+		mcs_nh("~MagCalibration")
+	{ }
+
+	/**
+	 * Plugin initializer. Constructor should not do this.
+	 */
+	void initialize(UAS &uas_)
+	{
+		PluginBase::initialize(uas_);
+		mcs_pub = mcs_nh.advertise<std_msgs::UInt8>("status", 2, true);
+	}
+
+	/**
+	 * This function returns message subscriptions.
+	 *
+	 * Each subscription made by PluginBase::make_handler() template.
+	 * Two variations:
+	 *  - With automatic decoding and framing error filtering (see handle_heartbeat)
+	 *  - Raw message with framig status (see handle_systemtext)
+	 */
+	Subscriptions get_subscriptions() {
+		return {
+			/* automatic message deduction by second argument */
+			make_handler(&MagCalStatusPlugin::handle_status),
+		};
+	}
+
+private:
+	ros::NodeHandle mcs_nh;
+	ros::Publisher mcs_pub;
+	uint8_t _rgCompassCalProgress[3];
+
+	void handle_status(const mavlink::mavlink_message_t *msg, mavlink::ardupilotmega::msg::MAG_CAL_PROGRESS &mp) {
+//		ROS_INFO_STREAM_NAMED("MagCalStatus", "MagCalStatus::handle_heartbeat: " << mp.to_yaml());
+		auto mcs = boost::make_shared<std_msgs::UInt8>();
+
+		// How many compasses are we calibrating?
+		int compassCalCount = 0;
+		for (int i=0; i<3; i++) {
+			if (mp.cal_mask & (1 << i)) {
+				compassCalCount++;
+			}
+		}
+
+		if (mp.compass_id < 3 && compassCalCount != 0) {
+			// Each compass gets a portion of the overall progress
+			_rgCompassCalProgress[mp.compass_id] = mp.completion_pct / compassCalCount;
+		}
+
+		mcs->data = (_rgCompassCalProgress[0] + _rgCompassCalProgress[1] + _rgCompassCalProgress[2]);
+
+		mcs_pub.publish(mcs);
+	}
+};
+
+}	// namespace std_plugins
+}	// namespace mavros
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(mavros::std_plugins::MagCalStatusPlugin, mavros::plugin::PluginBase)
